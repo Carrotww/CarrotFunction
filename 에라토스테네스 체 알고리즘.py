cdc0ffd006245056f329028faadcb5db3849b54c
Carrotww <104682050+Carrotww@users.noreply.github.com>
2022-06-21 23:00:48 +0900

    Create 에라토스테네스 체 알고리즘.py

---
+++ 에라토스테네스 체 알고리즘.py
@@ -0,0 +1,19 @@
+import math
+
+def find_prime(num: int) -> list:
+    num_list = [True for i in range(num + 1)]
+    prime_list = []
+
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num_list[i] == True:
+            j = 2
+            while i * j <= num:
+                num_list[i * j] = False
+                j += 1
+    for i in range(2, num + 1):
+        if num_list[i] == True:
+            prime_list.append(i)
+
+    return prime_list
+
+print(find_prime(25))
