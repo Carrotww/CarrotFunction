914638d11349868c5e2fe17f3a74b5bc6c844d13
Carrotww <104682050+Carrotww@users.noreply.github.com>
2022-06-24 00:09:34 +0900

    Update 소수찾기 푸는중.py

--- 소수찾기 푸는중.py
+++ 소수찾기 푸는중.py
@@ -4,9 +4,10 @@ from itertools import combinations
 def solution(num: str) -> int:
     count_prime = 0
     num_list = list()
-    for i in range(1, len(num) + 1):
-        # num_list.append(combinations(num, i))
+
+    for i in range(1, len(num) + 1):  # make permutations 1
         num_list += permutations(num, i)
+
     num_list = list(map(''.join, num_list))
     num_list = list(set([int(i) for i in num_list]))
     check_list = [True for i in range(len(num_list))]
@@ -22,17 +23,14 @@ def solution(num: str) -> int:
                     break
                 j += 1
 
-    # for i in range(len(num_list)):
-    #     sqrt = int(num_list[i] ** 0.5) + 1
-    #     for j in range(2, sqrt):
-    #         if num_list[i] % j == 0:
-    #             check_list[i] = False
-    #             continue
-    #
-    # for i in check_list:
-    #     if i == True:
-    #         count_prime += 1
-    return num_list, count_prime
+    for i in num_list:
+        if i >= 2:
+            pass
+
+
+
+    return num_list
 
 print(solution("17"))
 print(solution("011"))
+print(solution("3"))
