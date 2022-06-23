26a14d952db225b453dd7d36e5bf1ee543fe08e8
Carrotww <104682050+Carrotww@users.noreply.github.com>
2022-06-23 13:07:31 +0900

    Update 소수찾기 푸는중.py

--- 소수찾기 푸는중.py
+++ 소수찾기 푸는중.py
@@ -6,24 +6,32 @@ def solution(num: str) -> int:
     num_list = list()
     for i in range(1, len(num) + 1):
         # num_list.append(combinations(num, i))
-        num_list += combinations(num, i)
-    num_list = set(num_list)
+        num_list += permutations(num, i)
     num_list = list(map(''.join, num_list))
-    num_list = [int(i) for i in num_list]
+    num_list = list(set([int(i) for i in num_list]))
     check_list = [True for i in range(len(num_list))]
 
-    for i in range(len(num_list)):
-        sqrt = int(num_list[i] ** 0.5) + 1
-        for j in range(2, sqrt):
-            if num_list[i] % j == 0:
-                check_list[i] = False
-                continue
+    for i in num_list:
+        if i >= 2:
+            j = 2
+            while j <= int(i ** 0.5) + 1:
+                if i % j == 0:
+                    break
+                j += 1
+                if j == int(i ** 0.5):
+                    count_prime += 1
 
-    for i in check_list:
-        if i == True:
-            count_prime += 1
-
-    return num_list
+    # for i in range(len(num_list)):
+    #     sqrt = int(num_list[i] ** 0.5) + 1
+    #     for j in range(2, sqrt):
+    #         if num_list[i] % j == 0:
+    #             check_list[i] = False
+    #             continue
+    #
+    # for i in check_list:
+    #     if i == True:
+    #         count_prime += 1
+    return num_list, count_prime
 
 print(solution("17"))
 print(solution("011"))
